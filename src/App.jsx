75218782e7b318db6654d9b9068e821a631bc739
thebigjoe10 <jojosr001@gmail.com>
2023-09-20 03:48:23 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./assets/Header";
 import GpaInput from "./assets/GpaInput";
-
+import Footer from "./assets/Footer";
 const App = () => {
   return (
     <div>
       <Header />
       <GpaInput />
+      <Footer />
     </div>
   );
 };
