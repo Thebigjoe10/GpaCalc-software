a9997556eb95c18ce6f88f7f207cf3cf5a6d1ec7
thebigjoe10 <jojosr001@gmail.com>
2024-02-06 15:06:04 -0800

    update: main.jsx and public folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/client/public/sw.js');
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
